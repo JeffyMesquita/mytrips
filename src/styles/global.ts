ec8e8e5efa747e980f09cabc304545101ea71815
Jeferson Mesquita <je_2742@hotmail.com>
2022-02-04 23:46:29 -0300

    initial commit
    begin project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --highlight: #E20E8D;
+    --background: #030518;
+    --white: #EEEEEE;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -13,11 +25,22 @@ const GlobalStyle = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 

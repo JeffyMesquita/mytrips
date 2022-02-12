7604e6be9973298598ab620b780cbff15a4aaec6
Jeferson Mesquita <je_2742@hotmail.com>
2022-02-12 15:14:03 -0300

    att

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+import * as S from './styles'
+
 type Place = {
   id: string
   name: string
@@ -37,34 +39,37 @@ const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
   return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <CustomTileLayer />
+    <S.MapWrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        style={{ height: '100%', width: '100%' }}
+        minZoom={3}
+      >
+        <CustomTileLayer />
 
-      {places?.map(({ id, slug, name, location }) => {
-        const { latitude, longitude } = location
+        {places?.map(({ id, slug, name, location }) => {
+          const { latitude, longitude } = location
 
-        return (
-          <Marker
-            key={`place-${id}`}
-            position={[latitude, longitude]}
-            title={name}
-            eventHandlers={{
-              click: () => {
-                router.push(`/place/${slug}`)
-              }
-            }}
-          >
-            <Popup>
-              <span>{name}</span>
-            </Popup>
-          </Marker>
-        )
-      })}
-    </MapContainer>
+          return (
+            <Marker
+              key={`place-${id}`}
+              position={[latitude, longitude]}
+              title={name}
+              eventHandlers={{
+                click: () => {
+                  router.push(`/place/${slug}`)
+                }
+              }}
+            >
+              <Popup>
+                <span>{name}</span>
+              </Popup>
+            </Marker>
+          )
+        })}
+      </MapContainer>
+    </S.MapWrapper>
   )
 }
 
